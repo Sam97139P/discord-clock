f1c167461fae5a05cf32fc0d1f58b9a77c3bad93
Sam97139P <72031145+Sam97139P@users.noreply.github.com>
2022-08-06 14:45:34 -0400

    v1

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ const chalk = require('chalk');
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const { TIMEZONE, FORMAT, CHANNEL_ID, UPDATE_INTERVAL, BOT_TOKEN} = process.env;
+const { TIMEZONE, FORMAT, CHANNEL_ID, UPDATE_INTERVAL, BOT_TOKEN } = process.env;
 
 //'ready' event
 client.once('ready', () => {
-  //init time
-  const timeNow = moment().tz(TIMEZONE).format(FORMAT);
-  //define clockChannel
-  const clockChannel = client.channels.cache.get(CHANNEL_ID);
-  //initial update
-  clockChannel.edit({ name: `🕒 ${timeNow}` }, 'Clock update')
-    .catch(console.error);
-  //set the interval
-  setInterval(() => {
-    const timeNowUpdate = moment().tz(TIMEZONE).format(FORMAT);
-    clockChannel.edit({ name: `🕒 ${timeNowUpdate}` }, 'Clock update')
-      .catch(console.error);
-  }, UPDATE_INTERVAL);
-  //tells if it is ready
-	console.log(chalk.greenBright("[READY]"), `Logged in as ${client.user.tag} (${client.user.id}) at ${moment().format("DD MMMM YYYY, HH:mm:ss")}`);
+    //init time
+    const timeNow = moment().tz(TIMEZONE).format(FORMAT);
+    //define clockChannel
+    const clockChannel = client.channels.cache.get(CHANNEL_ID);
+    //initial update
+    clockChannel.edit({ name: `🕒 ${timeNow}` }, 'Clock update')
+        .catch(console.error);
+    //set the interval
+    setInterval(() => {
+        const timeNowUpdate = moment().tz(TIMEZONE).format(FORMAT);
+        clockChannel.edit({ name: `🕒 ${timeNowUpdate}` }, 'Clock update')
+            .catch(console.error);
+    }, UPDATE_INTERVAL);
+    //tells if it is ready
+    console.log(chalk.greenBright("[READY]"), `Logged in as ${client.user.tag} (${client.user.id}) at ${moment().format("DD MMMM YYYY, HH:mm:ss")}`);
 });
 
 //log in
